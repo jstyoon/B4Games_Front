54d5ed0157e28220785da23ea6b64ee57249f577
Bookwhale00 <Bookwhale00@gmail.com>
2023-05-12 20:21:07 +0900

    :memo: Docs: 게시글작성 api하는중

---
+++ js/posting.js
@@ -0,0 +1,31 @@
+const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "http://127.0.0.1:8000"
+
+async function postArticle() {
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    const image = document.getElementById("image").files[0]
+
+    const formdata = new FormData();
+
+    formdata.append('title', title)
+    formdata.append('content', content)
+    formdata.append('image', image)
+
+    let token = localStorage.getItem("access")
+
+    const response = await fetch(`${backend_base_url}/api/posts/`, {
+        method: 'POST',
+        headers: {
+            "Authorization" : `Bearer ${token}`
+        },
+        body: formdata
+    }
+    )
+    if (response.status==200) {
+        alert("작성완료")
+        window.location.replace(`${frontend_base_url}/`)
+    } else {
+        alert(response.status)
+    }
+}
