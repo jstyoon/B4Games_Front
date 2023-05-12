931c5e5911af130b819021019407b4816e8453cb
SungSuSon <thstjdtn05@gmail.com>
2023-05-12 22:45:42 +0900

    :Rename: profile html rename
    set test.html

--- js/home.js
+++ js/home.js
@@ -36,8 +36,10 @@ window.onload = async function loadArticles() {
     } else {
         dropdown_item_3 = document.getElementById("dropdown_item_3")
         dropdown_item_4 = document.getElementById("dropdown_item_4")
+        dropdown_item_5 = document.getElementById("dropdown_item_5")
         dropdown_item_3.style.display = "none"
         dropdown_item_4.style.display = "none"
+        dropdown_item_5.style.display = "none"
     }
 
     const articles = await getArticles()
