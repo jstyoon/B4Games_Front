ccf2023da8c9d36ec95c58c0db77087e5c41b6a2
SungSuSon <thstjdtn05@gmail.com>
2023-05-14 19:58:54 +0900

    :books: Docs: delete mapage
    마이페이지 삭제

--- js/home.js
+++ js/home.js
@@ -57,12 +57,10 @@ window.onload = async function loadArticles() {
         dropdown_item_3 = document.getElementById("dropdown_item_3")
         dropdown_item_4 = document.getElementById("dropdown_item_4")
         // dropdown_item_5 = document.getElementById("dropdown_item_5")
-        dropdown_item_6 = document.getElementById("dropdown_item_6")
         dropdown_item_8 = document.getElementById("dropdown_item_8")
         dropdown_item_3.style.display = "none"
         dropdown_item_4.style.display = "none"
         // dropdown_item_5.style.display = "none"
-        dropdown_item_6.style.display = "none"
         dropdown_item_8.style.display = "none"
     }
 
